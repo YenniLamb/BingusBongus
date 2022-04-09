f7bef8e606cd0397fa2915790851098d7a576af4
YenniLamb <47261678+YenniLamb@users.noreply.github.com>
2022-04-08 18:16:45 -0700

    first commit
    working bingus bongus

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+
+    public Text scoreText;
+
+    public GameObject playButton;
+
+    public AudioSource backGroundMusic;
+
+    public AudioClip soundEffect; 
+
+    public GameObject gameOver;
+    public GameObject getReady;
+
+    public bool gameEnd;
+
+    private int score;
+
+    private void Awake() {
+
+        // Application.targetFrameRate = 60;
+        //backGroundMusic = GetComponent<AudioSource>();
+
+        
+        gameOver.SetActive(false);
+        
+        getReady.SetActive(true);
+        Pause();
+    }
+
+    public void Play() {
+        score = 0;     //reset score to 0
+        scoreText.text = score.ToString();
+
+        backGroundMusic.Play();
+        //backGroundMusic.PlayOneShot(backGroundMusic, 0.7F);
+
+        playButton.SetActive(false);
+        getReady.SetActive(false);
+        gameOver.SetActive(false);
+        // Destroy(gameOver);
+
+
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Nets[] nets = FindObjectsOfType<Nets>();
+
+        for (int i = 0; i < nets.Length; i++) {
+            Destroy(nets[i].gameObject);
+        }
+
+    }
+
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+
+       
+
+    }
+
+    public void GameOver() {
+        //Debug.Log("Game over");
+        gameEnd = false;
+        //AudioSource.PlayClipAtPoint(soundEffect);
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        // backGroundMusic = GetComponent<AudioSource>();
+        // backGroundMusic.Play();
+        
+        if (gameEnd == false)
+        {
+            backGroundMusic.Stop();
+            backGroundMusic.PlayOneShot(soundEffect, 0.553f);
+            gameEnd = true;   
+            
+        }
+       
+
+        Pause();
+    }
+
+    public void IncreaseScore() {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,60 @@
+﻿//using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Text;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //public GameObject prefab;
+
+    public List<GameObject> objectsToSpawn;//= new List<GameObject>();
+
+    public bool isRandomized;
+
+    public float spawnRate = 1.5f;  // how often we want to spawn the object
+    public float netsMinHeight = -1.84f;
+    public float netsMaxHeight = 0.83f;
+    public float pipeMinHeight = -3.5f;
+    public float pipeMaxHeight = 0.5f;
+
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn()
+    {
+        int index = isRandomized ? Random.Range(0, objectsToSpawn.Count) : 0;
+
+        if (objectsToSpawn.Count > 0)
+        {
+            GameObject pipes = Instantiate(objectsToSpawn[index], transform.position, Quaternion.identity);
+            //Debug.Log("pipes: " + pipes.name); //print Nets(clone)
+
+            //Debug.Log("object index:   " + objectsToSpawn[index].name); //print nets
+            //Debug.Log("    Is pobject equal to string: " + objectsToSpawn[index].name.Equals("Nets"));
+
+            if (objectsToSpawn[index].name.Equals("Nets"))
+            {
+                pipes.transform.position += Vector3.up * Random.Range(netsMinHeight, netsMaxHeight);
+                //Debug.Log("true");
+            }
+            else if (objectsToSpawn[index].name.Equals("Pipes2 Variant"))
+            {
+                pipes.transform.position += Vector3.up * Random.Range(pipeMinHeight, pipeMaxHeight);
+               // Debug.Log("false");
+            }
+        }
+        //GameObject nets = Instantiate(prefab, transform.position, Quaternion.identity);
+        //nets.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+    }
+
+}
